d08b7f740f992549e71d5bcc3372d35ca559ad7a
Bhavani-Durgam <durgambhavani799@gmail.com>
2026-04-06 16:37:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,3 @@
-resource "aws_vpc" "name" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-      Name = "test_vpp"
-    }
-  
-}
-
-
-resource "aws_subnet" "name2" {
-    vpc_id = aws_vpc.name.id
-    cidr_block = "10.0.0.0/24"
-    tags = {
-      Name = "mysubnet1"
-    }
-  
-}
 resource "aws_instance" "example_server" {
   ami           = "ami-01b14b7ad41e17ba4" # Update with a valid AMI ID for your region
   instance_type = "t2.micro"
